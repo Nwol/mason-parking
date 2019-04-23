c0a1448320dd6c419c8c36a647f37fd619f6b6b0
Nahom Woldeab <nahom@opphub.net>
2019-04-23 10:53:37 -0400

    Added jest unit tests for implementation

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { create } from "react-test-renderer"; 
 import App from './App';
+import ViewScreen from './ViewScreen';
+import Login from './Login';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('view screen', () => {
+  test('matches the snapshot', () => {
+    const component = create (
+      <ViewScreen/>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
+
+describe('authenticated viewscreen', () => {
+  test('matches the snapshot', () => {
+    const component = create (
+      <ViewScreen user={true}/>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
+
+describe('login screen', () => {
+  test('matches the snapshot', () => {
+    const component = create (
+      <Login/>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 });
